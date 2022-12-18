d6b4747b0319c2355bf33d18f3290084629e112b
prateekb1912 <prateekb1912@gmail.com>
2022-12-18 23:04:00 +0530

    Scrape data for both innings whole

--- espnscraper/espnscraper/items.py
+++ espnscraper/espnscraper/items.py
@@ -7,6 +7,7 @@ import scrapy
 
 
 class BallDataItem(scrapy.Item):
+    inning = scrapy.Field()
     overNumber = scrapy.Field()
     ballNumber = scrapy.Field()
     totalRuns = scrapy.Field()
--- espnscraper/espnscraper/spiders/espnscraper.py
+++ espnscraper/espnscraper/spiders/espnscraper.py
@@ -7,22 +7,23 @@ class ESPNCommentarySpider(scrapy.Spider):
 
     allowed_domains = ['espncricinfo.com']
 
-    start_urls = ['https://hs-consumer-api.espncricinfo.com/v1/pages/match/comments?lang=en&seriesId=1324623&matchId=1324629&inningNumber=2&commentType=ALL&sortDirection=ASC']
+    start_urls = [f'https://hs-consumer-api.espncricinfo.com/v1/pages/match/comments?lang=en&seriesId=1324623&matchId=1324629&inningNumber={inn_num}&commentType=ALL&sortDirection=ASC&fromInningOver={over_num}' for inn_num in range(1,3) for over_num in range(1,20,2)]
 
     def parse(self, response):
         results = json.loads(response.body)
         
-        next_over = results['nextInningOver']
-
         comments = results['comments']
+
+        print(results['nextInningOver'])
 
         for comment in comments:
             item = BallDataItem()
 
+            item['inning'] = comment['inningNumber']
             item['overNumber'] = comment['overNumber']
             item['ballNumber'] = comment['ballNumber']
             item['totalRuns'] = comment['totalRuns']
             item['inningsRuns'] = comment['totalInningRuns']
             item['inningsWickets'] = comment['totalInningWickets']
-            
+
             yield item
